c34803fa121e6f7b1b1cd5a27df78333657d3d8e
per2 <diegoalexwilliams33@gmail.com>
2021-09-09 09:46:23 -0700

    Android lifecycle methods overridden

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.example.stopwatch
 
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Chronometer
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var timer : Chronometer
+    lateinit var startStopButton : Button
+    lateinit var resetButton : Button
+
+    companion object{
+        //all static constants go here
+        val TAG = "MainActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause: ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    fun wireWidgits(){
+        timer = findViewById(R.id.chronometer_main_stopwatch)
+        startStopButton = findViewById(R.id.button_main_startStop)
+        resetButton = findViewById(R.id.button_main_reset)
     }
 }
